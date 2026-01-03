e235ec038556a5c2779d641ca2109ae4d10dab2c
Moushumi Maitra <moushumimaitra23@gmail.com>
2026-01-03 21:29:25 +0530

    user authentication done

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 const express = require('express');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
 const connectDB = require('./db/db');
 const userRoutes = require('./routes/user.routes');
 
@@ -12,6 +13,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.use('/users', userRoutes);
 
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { body } = require('express-validator');
 const userController = require('../controllers/user.controller');
+const authMiddleware = require('../middlewares/auth.middleware');
 
 router.post('/register', [
     body('email').isEmail().withMessage('Invalid email'),
@@ -13,5 +14,9 @@ router.post('/login', [
     body('email').isEmail().withMessage('Invalid email'),
     body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long')
 ], userController.loginUser);
+
+router.get('/logout', authMiddleware.authUser, userController.logoutUser);
+
+router.get('/profile', authMiddleware.authUser, userController.getUserProfile)
 
 module.exports = router;
