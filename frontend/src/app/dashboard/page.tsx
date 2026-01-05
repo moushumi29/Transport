7eb0bab06a3dca41c624ed317e84b6563b527943
Moushumi Maitra <moushumimaitra23@gmail.com>
2026-01-05 20:59:48 +0530

    small fix

---
+++ frontend/src/app/dashboard/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import React from 'react';
+import Header from '@src/components/dashboard/Header'
+
+const DashboardPage = () => {
+  return (
+    <div className=''>
+      <Header />
+    </div>
+  )
+}
+
+export default DashboardPage
---
+++ frontend/src/components/dashboard/Header.tsx
@@ -0,0 +1,37 @@
+import { ChevronDown, TruckElectric } from 'lucide-react'
+import React from 'react'
+import { Baloo_2 } from 'next/font/google'
+
+const baloo2 = Baloo_2({
+    subsets: ['latin'],
+    weight: ['400', '500', '600', '700'], // you can choose weights you want
+})
+
+const Header = () => {
+    return (
+        <div className='flex justify-between px-8 py-4 border-b border-gray-200 items-center shadow-md'>
+            <div className={`${baloo2.className} text-green-500 text-3xl flex items-center gap-1 font-semibold`}>
+                <TruckElectric size={36} /> CAgent
+            </div>
+            <div className="flex items-center gap-2">
+                {/* Profile Image */}
+                <div className="flex items-center gap-2 bg-white p-2 rounded-full shadow-md">
+                    <img
+                        src="/globe.svg" // replace with your image path or URL
+                        alt="User"
+                        className="w-8 h-8 rounded-full object-cover"
+                    />
+                </div>
+
+
+                {/* User Name */}
+                <span className="text-zinc-500 font-semibold">Douglas Steele</span>
+
+                {/* Dropdown Icon */}
+                <ChevronDown className="w-4 h-4 text-gray-500" />
+            </div>
+        </div>
+    )
+}
+
+export default Header
